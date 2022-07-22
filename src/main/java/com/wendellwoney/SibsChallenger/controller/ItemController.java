e01a08a157509251fd963be4dfd41c6a4cb44391
Wendell <wendell@ibtech.inf.br>
2022-07-22 11:49:54 -0300

    init Item Controller

--- src/main/java/com/wendellwoney/SibsChallenger/controller/ItemController.java
+++ src/main/java/com/wendellwoney/SibsChallenger/controller/ItemController.java
@@ -3,8 +3,11 @@ package com.wendellwoney.SibsChallenger.controller;
 import com.wendellwoney.SibsChallenger.dto.ItemDto;
 import com.wendellwoney.SibsChallenger.dto.ResponseDto;
 import com.wendellwoney.SibsChallenger.dto.ResponseListDto;
+import com.wendellwoney.SibsChallenger.service.ItemServiceInterface;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,32 +16,35 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ItemController implements ItemControllerInterface {
 
+    @Autowired
+    private ItemServiceInterface itemService;
+
     @Override
-    @GetMapping("/itens")
+    @GetMapping(value = "/itens", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseListDto> getAll() {
-        return null;
+        return ResponseEntity.ok(itemService.getAll());
     }
 
     @Override
-    @GetMapping("item/{id}")
+    @GetMapping(value =  "item/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseDto> get() {
         return null;
     }
 
     @Override
-    @PostMapping("item")
+    @PostMapping(value = "item", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseDto> create(ItemDto item) {
         return null;
     }
 
     @Override
-    @PutMapping("item")
+    @PutMapping(value = "item", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseDto> update(ItemDto item) {
         return null;
     }
 
     @Override
-    @DeleteMapping("item/{id}")
+    @DeleteMapping(value = "item/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Boolean> delete(Long item) {
         return null;
     }
