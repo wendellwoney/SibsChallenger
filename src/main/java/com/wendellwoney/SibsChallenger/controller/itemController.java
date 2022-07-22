ffa6a77c39a930820f0f46bd6d367fef11db6a0b
Wendell <wendell@ibtech.inf.br>
2022-07-22 01:08:01 -0300

    handle log4j2

--- src/main/java/com/wendellwoney/SibsChallenger/controller/itemController.java
+++ src/main/java/com/wendellwoney/SibsChallenger/controller/itemController.java
@@ -1,5 +1,7 @@
 package com.wendellwoney.SibsChallenger.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${api.path}")
 public class itemController {
 
+    private static final Logger logger = LogManager.getLogger(itemController.class);
+
     @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> hello() {
+        logger.error("Hey, This is a error!");
         return ResponseEntity.ok("Teste");
     }
 }
