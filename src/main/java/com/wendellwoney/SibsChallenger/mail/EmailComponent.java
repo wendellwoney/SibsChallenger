c01b1b3aaa0dc2fa0d6b8bd9a03c91d20bd954d3
Wendell <wendell@ibtech.inf.br>
2022-07-23 18:36:37 -0300

    mail config

---
+++ src/main/java/com/wendellwoney/SibsChallenger/mail/EmailComponent.java
@@ -0,0 +1,25 @@
+package com.wendellwoney.SibsChallenger.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailComponent {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(this.from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
